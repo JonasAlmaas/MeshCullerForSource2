374b8ee226729a1d0a324310a519348b8c694ac0
Jonas Almås <jonasalmaas7@gmail.com>
2024-01-19 19:50:47 +0100

    Rename project to MeshCullerSourec2

---
+++ MeshCullerForSource2/Program.cs
@@ -0,0 +1,16 @@
+﻿using Datamodel;
+
+namespace MeshCullerForSource2;
+
+internal class Program
+{
+	static void Main(string[] args) {
+		Console.WriteLine($"Mesh Culler Source 2 - Build {GetBuildNo()}");
+
+		Console.WriteLine("Mesh Culler Source 2 - Completed!");
+	}
+	
+	public static string GetBuildNo() {
+		return "0.0.1";
+	}
+}
