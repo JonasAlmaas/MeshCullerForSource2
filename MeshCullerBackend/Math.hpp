15c6c5e9b4a2b4bf88a7ec0c205e399b774337b3
bonjorno7 <jorijndegraaf@gmail.com>
2024-01-23 00:46:31 +0100

    Add helper to convert matrix from FBX to GLM
    Time will tell whether it works as intended.

--- MeshCullerBackend/Math.hpp
+++ MeshCullerBackend/Math.hpp
@@ -1,8 +1,18 @@
 #pragma once
 
-#include <iostream>
-
+#include <fbxsdk.h>
 #include <glm/glm.hpp>
+
+inline glm::mat4 fbxMat2glmMat(const FbxMatrix& mat)
+{
+	// TODO: Test whether it should be row-wise or column-wise.
+	return {
+		mat[0][0], mat[1][0], mat[2][0], mat[3][0],
+		mat[0][1], mat[1][1], mat[2][1], mat[3][1],
+		mat[0][2], mat[1][2], mat[2][2], mat[3][2],
+		mat[0][3], mat[1][3], mat[2][3], mat[3][3]
+	};
+}
 
 bool rayIntersectsTriangle(const glm::vec3& origin, const glm::vec3& direction, const glm::vec3& a, const glm::vec3& b, const glm::vec3& c)
 {
